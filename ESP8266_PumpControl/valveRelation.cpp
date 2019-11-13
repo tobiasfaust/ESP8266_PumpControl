56573e4fefb4505994a851311b82eba552b0e93a
Tobias Faust <tobias.faust@gmx.net>
2019-11-13 08:05:12 +0100

    default relations now disabled

--- ESP8266_PumpControl/valveRelation.cpp
+++ ESP8266_PumpControl/valveRelation.cpp
@@ -124,8 +124,8 @@ void valveRelation::LoadJsonConfig() {
 
   if (loadDefaultConfig) {
     Serial.println("lade Relations DefaultConfig");
-    AddRelation(true, "testhost/TestVirtualValve1", 203);
-    AddRelation(true, "testhost/TestVirtualValve2", 203);
+    AddRelation(false, "testhost/TestVirtualValve1", 203);
+    AddRelation(false, "testhost/TestVirtualValve2", 203);
   }
 }
 
