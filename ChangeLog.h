212f3635f242ccef3ad4708e7565e78d6ad34594
tobiasfaust <tobias.faust@gmx.net>
2020-06-29 14:05:05 +0200

    bugfix Nullpointer
    Bugfix: Nullpointer to Hardwaredevice if multiple hardware devices are defined

--- ChangeLog.h
+++ ChangeLog.h
@@ -1,8 +1,9 @@
 Release 2.4.4:  -
   - Feature: Issue #9: MQTT Client ID now configurable
   - MQTT now reconnect after DeviceName has been changed
-  - MQTT LastWillTopic as device status configured: state [Offline|Online]
-
+  - MQTT LastWillTopic as device status configured by topic "/state [Offline|Online]"
+  - Publish Release and Version after MQTT Connect by topic "/version"
+  - Bugfix: Nullpointer to Hardwaredevice if multiple hardware devices are defined
 Release 2.4.3:
   - Bugfixing Automatische Releaseverteilung
   - Überarbeitung Github Workflow mit automatischer Releaseerstellung 
