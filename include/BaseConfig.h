a94a90c9e288585c6fbe7c2e0580168ec152baa5
tobiasfaust <tobias.faust@gmx.net>
2023-08-15 15:10:21 +0200

    move header files to include folder

--- include/BaseConfig.h
+++ include/BaseConfig.h
@@ -20,30 +20,30 @@ class BaseConfig {
     void      GetWebContent(uint8_t* buffer, std::shared_ptr<uint16_t> processedRows, size_t& currentRow, size_t& len, size_t& maxLen);
     void      loop();
 
-    const uint8_t& GetPinSDA()      const {return pin_sda;}
-    const uint8_t& GetPinSCL()      const {return pin_scl;}
-    const uint8_t& GetPin1Wire()      const {return pin_1wire;}
-    const uint8_t& GetI2cOLED()     const {return i2caddress_oled;}
-    const bool&    EnabledOled()    const {return enable_oled;}
-    const uint8_t& GetOledType()   const {return oled_type;}
-    const bool&    Enabled1Wire()    const {return enable_1wire;}
-    const String&  GetMqttServer()  const {return mqtt_server;}
+    const uint8_t&  GetPinSDA()      const {return pin_sda;}
+    const uint8_t&  GetPinSCL()      const {return pin_scl;}
+    const uint8_t&  GetPin1Wire()      const {return pin_1wire;}
+    const uint8_t&  GetI2cOLED()     const {return i2caddress_oled;}
+    const bool&     EnabledOled()    const {return enable_oled;}
+    const uint8_t&  GetOledType()   const {return oled_type;}
+    const bool&     Enabled1Wire()    const {return enable_1wire;}
+    const String&   GetMqttServer()  const {return mqtt_server;}
     const uint16_t& GetMqttPort()   const {return mqtt_port;}
-    const String&  GetMqttUsername()const {return mqtt_username;}
-    const String&  GetMqttPassword()const {return mqtt_password;}
+    const String&   GetMqttUsername()const {return mqtt_username;}
+    const String&   GetMqttPassword()const {return mqtt_password;}
     const String&   GetMqttBasePath()  const {return mqtt_basepath;}
-    const String&  GetMqttRoot()    const {return mqtt_root;}
-    const bool&    UseRandomMQTTClientID() const { return mqtt_UseRandomClientID; }
-    const uint8_t& Get3WegePort()   const {return ventil3wege_port;}
-    const bool&    Enabled3Wege()   const {return enable_3wege;}
-    const uint8_t& GetMaxParallel() const {return max_parallel;}
+    const String&   GetMqttRoot()    const {return mqtt_root;}
+    const bool&     UseRandomMQTTClientID() const { return mqtt_UseRandomClientID; }
+    const uint8_t&  Get3WegePort()   const {return ventil3wege_port;}
+    const bool&     Enabled3Wege()   const {return enable_3wege;}
+    const uint8_t&  GetMaxParallel() const {return max_parallel;}
     const uint16_t& GetKeepAlive()   const {return keepalive;}
-    const uint8_t& GetDebugLevel()   const {return debuglevel;}
-    String    GetReleaseName();
-    void      InstallRelease(uint32_t ReleaseNumber);
-    void      RefreshReleases();
+    const uint8_t&  GetDebugLevel()   const {return debuglevel;}
+    String          GetReleaseName();
+    void            InstallRelease(uint32_t ReleaseNumber);
+    void            RefreshReleases();
 
-    size_t    getFragmentation();
+    size_t          getFragmentation();
     
     void handleOneHtmlRow(const size_t& curRow, 
        std::shared_ptr<uint16_t> processedRows, 
